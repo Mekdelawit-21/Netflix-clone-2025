ef9df0dea2ee1a0f112274bfa1aeca8939a49f3a
Mekdelawit-21 <mekdih.2116@gmail.com>
2025-05-12 17:37:52 +0300

    axios and request

---
+++ netflix-clone/src/utils/requests.jsx
@@ -0,0 +1,16 @@
+// ***API_KEY = "9ffae6538c4854ebf385f006a0a8bcc5";***
+
+const API_KEY = "process.env.REACT_APP_API_KEY";
+const requests = {
+  fetchTrending: "/trending/all/week?api_key=${API_KEY}&language=en-us",
+  fetchNetflixOriginals: "/discover/tv?api_key=${API_KEY}&with_networks=213",
+  fetchTopRatedMovies: "/movie/top_rated?api_key=${API_KEY}&language=en-us",
+  fetchActionMovies: "/discover/movie?api_key=${API_KEY}&with_genres=28",
+  fetchComedyMovies: "/discover/movie?api_key=${API_KEY}&with_genres=35",
+  fetchHorrorMovies: "/discover/movie?api_key=${API_KEY}&with_genres=27",
+  fetchRomanceMovies: "/discover/movie?api_key=${API_KEY}&with_genres=10749",
+  fetchDocumentaries: "/discover/movie?api_key=${API_KEY}&with_genres=99",
+  fetchTvShow: 'tv/popular?api_key=${API_KEY}&language=en-us&page=1'
+};
+
+export default requests;
