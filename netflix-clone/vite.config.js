b11a20a32c17bde6c92cbb7d6b7a68cf70f76561
Mekdelawit-21 <mekdih.2116@gmail.com>
2025-05-18 11:30:37 +0300

    gh-pages

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Netflix-clone-2025",
+  build: {
+    outDir: 'dist',       
+    sourcemap: true,  
+  },
 });
